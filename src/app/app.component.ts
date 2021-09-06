4ff319b58b456837e012a67599cc20de0c96f2cf
Yoke24 <90189373+Yoke24@users.noreply.github.com>
2021-09-06 16:34:05 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { title } from 'process';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'covid19-tracker';
 }
+
+console.log(title);
